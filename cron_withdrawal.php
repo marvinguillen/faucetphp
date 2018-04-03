76e3a991f73ce3756f7a1e6bb948a8aa7fd70b77
cesardanielhg <cesardanielhg@gmail.com>
2018-04-03 12:48:10 -0600

    COMMENT ALL FUNCTIONS

--- cron_withdrawal.php
+++ cron_withdrawal.php
@@ -3,8 +3,11 @@ error_reporting(E_ALL & ~ E_NOTICE); ini_set('display_errors', true);
 
 require_once "maincore.php";
 require_once "includes/dbconnector.class.php";
-require_once "includes/AsmoneyAPI.php";
+//require_once "includes/AsmoneyAPI.php";
 
+
+echo "HOLA"
+/*
 function ChangetoMili($amount,&$currency) {
 	switch ($currency)
 	{		
@@ -173,4 +176,5 @@ if (count($btcamounts) > $requestcount)
 		    {		echo "An Error Occured";		}	
 		}
 }
+*/
 ?>
