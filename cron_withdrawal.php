c931e724c041a18b2e9be02940e266165a3d4d2a
Cesar Hernandez <cesar.hernandez@laureate.net>
2018-05-03 22:35:36 -0600

    Solving error with log table

--- cron_withdrawal.php
+++ cron_withdrawal.php
@@ -111,9 +111,8 @@ if (count($btcamounts) >= $requestcount)
 		}
 
 	    $db->query("insert into tbl_cronjob_history 
-			(  run_date, success,total_amount,total_transfers, fee, hash_transfer ) 
-	 values (".$run_date.",1 ,".$total_amount.",".$requestcount.",".$transfer_fee.","'rffrf'
-	 " )
+			(  run_date, success,total_amount,total_transfers, fee,  hash_transfer ) 
+	 values (".$run_date.",1 ,".$total_amount.", ".$requestcount.",".$transfer_fee.",".$hash_transfer." )
 	 		 ");
 
 	    echo "</br><h3>".$requestcount. " Withdrawals has been proceessed with hash number:".$hash_transfer."</h3>" ;
